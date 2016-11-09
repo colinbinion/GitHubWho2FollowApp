99da50beaeec2cf4736ab7f7f5ba1a1defba865d
colinbinion <colin.binion@me.com>
2016-11-08 19:54:01 -0700

    messing with response

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # GitHubWho2FollowApp
+
+https://github.com/colinbinion/GitHubWho2FollowApp.git
+
+Core features:
+
+  -On startup, load accounts data from the API and display 3 suggestions
+  -On clicking "Refresh", load 3 other account suggestions into the 3 rows
+  -On click 'x' button on an account row, clear only that current account and display another
+  -Each row displays the account's avatar and links to their page
+
+Using GitHub API for User data.
+  http://devdocs.io/jquery/jquery.getjson
+
+  
---
+++ RxJs.js
@@ -0,0 +1,16 @@
+// requesting the "what" and "when" for observable
+// the just operator converts an item into an Observable that emits that item
+var requestStream = Rx.Observable.just('https://api.github.com/users');
+
+// subscribe makes something happen when the value is emitted
+requestStream.subscribe(function(requestUrl) {
+  // similar to using a jquery ajax callback to handle the request Rx deals with asynchronous data streams.
+  //  Rx.Observable.create() creates a custom stream by explicitly informing "subscriber" about data events (onNext()) or errors (onError()). wrapping the jquery ajax promise makes the promise an observable?
+
+// request
+  var responseStream = Rx.Observable.create(function (observer) {
+    jQuery.getJSON(requestUrl, function(responseData) {
+
+    })
+  })
+})
