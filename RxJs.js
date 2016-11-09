b5795a28f00c6785b8e1382a6ce378a2858d305a
colinbinion <colin.binion@me.com>
2016-11-08 21:41:33 -0700

    req/res stream update

--- RxJs.js
+++ RxJs.js
@@ -6,23 +6,12 @@ var requestStream = Rx.Observable.just('https://api.github.com/users');
 requestStream.subscribe(function(requestUrl) {
   // similar to using a jquery ajax callback to handle the request Rx deals with asynchronous data streams.
   //  Rx.Observable.create() creates a custom stream by explicitly informing "subscriber" about data events (onNext()) or errors (onError()). wrapping the jquery ajax promise makes the promise an observable?
-
-// request
-  var responseStream = Rx.Observable.create(function (observer) {
-    jQuery.getJSON(requestUrl)
-    .done(function(response) {
-      observer.onNext(response);
-    })
-    .fail(function(jqXHR, status, error) {
-      observer.onError(error);
-    })
-    .always(function() {
-      observer.onCompleted();
-    });
+// flatMap "flattens" a metastream, by emitting on the "trunk" stream everything that will be emitted on "branch" streams. the response stream is defined according to request stream. it is a response stream.
+var responseStream = requestStream
+  .flatMap(function(requestUrl) {
+    return Rx.Observable.fromPromise(jQuery.getJSON(requestUrl));
   });
 
   responseStream.subscribe(function(response) {
-// do something with the response
-
+    // render `response` to the DOM placeholder
   });
-})
